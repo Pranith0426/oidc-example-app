21b71bf11ccd26f0d842c877f364913cbe32bced
Pranith0426 <pranithprani.s5@gmail.com>
2025-02-13 20:20:54 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -8,10 +8,10 @@ const cfg = {
     },
   },
   oidc: {
-    oidc_provider_url: process.env['OIDC_PROVIDER_URL'] ?? 'http://localhost:3000',
-    client_id: process.env['OIDC_CLIENT_ID'] ?? 'exampleAppClientId',
+    oidc_provider_url: process.env['https://oidc.dnanexus.com'] ?? 'http://localhost:3000',
+    client_id: process.env['0oa2bxpkld5QqeOSC0h8'] ?? 'exampleAppClientId',
     client_secret: process.env['OIDC_CLIENT_SECRET'] ?? 'exampleAppClientSecret',
-    redirect_url: process.env['OIDC_REDIRECT_URL'] ?? `http://localhost:${process.env['APP_PORT'] ?? 3333}/callback`,
+    redirect_url: process.env['Jo8YhXXAT5NexcMb-CPcZYjpk8We1V2sH9aScZaohN0Ar-Cr8NKB3sXuPnl7XjIk'] ?? `http://localhost:${process.env['APP_PORT'] ?? 3333}/callback`,
     scopes: process.env['OIDC_SCOPES'] ?? 'user_id name email' //Note: "openid" scope is required and added by default
   },
   jwks: {
